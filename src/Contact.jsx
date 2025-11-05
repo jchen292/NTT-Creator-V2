46a11bbb7e1ee1aa191e251a368c6d89706af465
Jordan Chen <36782603+jchen292@users.noreply.github.com>
2025-11-05 14:24:21 -0600

    Update pages

---
+++ src/Contact.jsx
@@ -0,0 +1,15 @@
+
+
+function Contact() {
+    return (
+            <>
+            <h1>Contact Page</h1>
+            <p>Jordan Chen <br /> 
+            Email: <a href = "mailto:jordanchen13@yahoo.com">jordanchen13@yahoo.com</a>
+            </p> 
+            </>
+    )
+
+}
+
+export default Contact;
